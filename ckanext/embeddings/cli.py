b8e5f2f69194d5c940bf87d9deca2cdcbca034cf
amercader <amercadero@gmail.com>
2024-01-11 12:02:04 +0100

    CLI command to perform semantic searches

--- ckanext/embeddings/cli.py
+++ ckanext/embeddings/cli.py
@@ -2,13 +2,12 @@ from datetime import datetime, timezone
 import sys
 import uuid
 
-
 import click
-
 
 from ckan import model
 from ckan.plugins import toolkit
 
+from ckanext.embeddings.lib import get_embeddings_backend
 
 
 def get_commands():
@@ -35,8 +34,33 @@ def similar(dataset_id: str, limit: int):
         toolkit.error_shout(f"Dataset not found: {dataset_id}")
         sys.exit(1)
 
-    print(f"Similar datasets to {dataset_id}")
-    print("---------------")
+    msg = f"Similar datasets to {dataset_id}"
+    print(msg)
+    print("-" * len(msg))
     for r in result:
+
+        print(f"{r['id']} - {r['title']}")
+
+
+@embeddings.command()
+@click.argument("query", type=str)
+@click.option("-l", "--limit", type=int, default=10)
+def search(query: str, limit: int):
+    """Shows the closest dataset to the provided search term"""
+
+    backend = get_embeddings_backend()
+    query_embedding = backend.get_embedding_for_string(query)
+    search_params = {}
+    search_params["defType"] = "lucene"
+    # TODO: config field name
+    search_params["q"] = f"{{!knn f=vector topK={limit}}}{list(query_embedding)}"
+
+    result = toolkit.get_action("package_search")({"ignore_auth": True}, search_params)
+
+    msg = f"Closest datasets to '{query}'"
+    print(msg)
+    print("-" * len(msg))
+
+    for r in result["results"]:
 
         print(f"{r['id']} - {r['title']}")
